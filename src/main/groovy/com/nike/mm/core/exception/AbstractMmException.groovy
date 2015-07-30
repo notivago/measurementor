f66707138ade7e43e0b9ac3fc4589e45c8b966d7
Leonardo Bueno Postacchini <lpostacc@teksystems.com>
2015-07-30 17:27:01 -0400

    Work around the "VerifyError: Bad <init>" issue.
    For reference: https://issues.apache.org/jira/browse/GROOVY-6951

--- src/main/groovy/com/nike/mm/core/exception/AbstractMmException.groovy
+++ src/main/groovy/com/nike/mm/core/exception/AbstractMmException.groovy
@@ -1,5 +1,8 @@
 package com.nike.mm.core.exception
 
+import groovy.transform.CompileStatic;
+
+@CompileStatic
 abstract class AbstractMmException extends RuntimeException {
 
     AbstractMmException(final String message) {
